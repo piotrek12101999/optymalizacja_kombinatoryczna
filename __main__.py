2e68b1ac6b7e0e0dcf7409fc186570af61ce8911
Piotr Świątek <piotr.swiatek.1999@gmail.com>
2021-06-12 16:32:22 +0200

    add best result find

--- __main__.py
+++ __main__.py
@@ -2,9 +2,31 @@ from algorithms.ACO.main import main_aco
 from algorithms.Greedy.main import main_greedy
 from utils.plot import plot
 
+
+def get_best_result(file_name, attempts):
+    best_result = {}
+    for _ in range(attempts):
+        points, path, cost = main_aco(file_name)
+        if 'cost' in best_result:
+            if cost < best_result['cost']:
+                best_result['points'] = points
+                best_result['path'] = path
+                best_result['cost'] = cost
+        else:
+            best_result['points'] = points
+            best_result['path'] = path
+            best_result['cost'] = cost
+
+    return best_result
+
+
 if __name__ == "__main__":
     file = "data_sets/berlin52.txt"
+
     # points, path, cost = main_greedy(file)
-    points, path, cost = main_aco(file)
-    plot(points, path)
-    print(cost)
+    # plot(points, path)
+
+    best_result = get_best_result(file, 2)
+    print(best_result['cost'])
+    plot(best_result['points'], best_result['path'])
+
